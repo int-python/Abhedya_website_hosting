e71ce658c76c027a80eb76d8e3cbfa13e4e32706
int-python <2014pceeeaksahy015@poornima.org>
2021-03-26 17:58:38 +0530

    Updates

--- src/Components/BlogPage/EventCard.jsx
+++ src/Components/BlogPage/EventCard.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "./EventCard.css";
-import event from './images/event.jpeg';
-import event_link from './images/event_link.PNG';
-
+import Event from '../../Assets/Event.jpg';
 function EventCard () {
     return (
         <>
             <section className="event_card_container ">
-                <img className="event_img" src={event} alt="image"/>
-                <a href="#">
-                    <img className="event_registration_link" src={event_link} alt="event_registration_link"/>
-                </a>
-                {/* <a className="event_registor_button" href="#">REGISTOR NOW</a> */}
+                <img className="event_img" src={Event} alt="event image"/>
+                <a className="event_registor_button" href="http://bit.ly/DCSCTF2021">REGISTOR NOW</a>
             </section>
         </>
     );
